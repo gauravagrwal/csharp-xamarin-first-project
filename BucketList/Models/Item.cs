f6534f56a9f77d20410a02b57f4be15bb39a54e3
Gaurav Agrawal <gauravag.1005@gmail.com>
2021-08-10 22:14:43 +0530

    Module 2 Completed!

--- BucketList/Models/Item.cs
+++ BucketList/Models/Item.cs
@@ -1,11 +1,10 @@
-﻿using System;
-
-namespace BucketList.Models
+﻿namespace BucketList.Models
 {
     public class Item
     {
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
